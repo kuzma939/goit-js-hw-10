67ada8d456344438087680169c7c64b9ecc57bd3
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2024-04-17 12:09:43 +0200

    Update 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,7 +1,7 @@
-import flatpickr from 'flatpickr';
-import 'flatpickr/dist/flatpickr.min.css';
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
 import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
+import "izitoast/dist/css/iziToast.min.css";
 
  const dateTime = document.querySelector('input#datetime-picker');
   const startButton = document.querySelector('button');
